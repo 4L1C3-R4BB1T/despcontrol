b364bef14ddfce72046b6f766bc5f6c8b1b81ab2
Livia <livinha.guimaraes.013@gmail.com>
2024-06-15 19:17:06 -0300

    feat: Adicionando criação de despesa

--- routes/usuario_routes.py
+++ routes/usuario_routes.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
+from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 
+from dtos.nova_despesa import NovaDespesaDTO
+from models.despesa_model import Despesa
 from models.usuario_model import Usuario
+from repositories.categoria_repo import CategoriaRepo
 from repositories.despesa_repo import DespesaRepo
-from repositories.usuario_repo import UsuarioRepo
 from util.auth import obter_usuario_logado
 
 
@@ -15,12 +18,14 @@ templates = Jinja2Templates(directory = "templates")
 @router.get("/usuario/despesas")
 def get_root(request: Request, usuario_logado: Usuario = Depends(obter_usuario_logado)):
     despesas = DespesaRepo.obter_todos_por_usuario(usuario_logado.id)
+    categorias = CategoriaRepo.obter_todos()
     return templates.TemplateResponse(
         "despesas.html",
         {
             "request": request,
             "usuario": usuario_logado,
             "despesas": despesas,
+            "categorias": categorias,
         },
     )
 
@@ -34,3 +39,13 @@ def get_root(request: Request, usuario_logado: Usuario = Depends(obter_usuario_l
             "usuario": usuario_logado,
         },
     )
+
+
+@router.post("/usuario/post_cadastro_despesa", response_class=JSONResponse)
+async def post_cadastro(despesa: NovaDespesaDTO, usuario_logado: Usuario = Depends(obter_usuario_logado)):
+    despesa_data = despesa.model_dump()
+    despesa_data["id_usuario"] = usuario_logado.id
+    nova_despesa = DespesaRepo.inserir(Despesa(**despesa_data))
+    if not nova_despesa or not nova_despesa.id:
+        raise HTTPException(status_code=400, detail="Erro ao cadastrar despesa.")
+    return {"redirect": {"url": "/usuario/despesas"}}
